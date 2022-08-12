858ed8e54895750921f761029a4fe2327cfa1c31
liuzhiyu <20051212183@stu.xidian.edu.cn>
2022-08-12 17:44:49 +0800

    首次提交测试git上传功能

---
+++ src/awdad.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Locale;
+import java.util.*;
+import java.util.Scanner;
+
+public class awdad {
+
+    public static void main(String[] args) {
+//        Map<Integer,Integer> hashmap=new HashMap<Integer,Integer>();
+//        hashmap.put(1,1);
+//
+//        hashmap.put(3,3);
+//        hashmap.put(2,5);
+//        for(int i:hashmap.keySet()){
+//
+//            System.out.println(i+" "+hashmap.get(i));
+//        }
+        Set<Integer> set=new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(10);
+        set.add(3);
+        for (int i:set){
+            System.out.println(i);
+        }
+
+
+
+
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
